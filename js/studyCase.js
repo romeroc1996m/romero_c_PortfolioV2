027ea5c5b33b8ead04f19d23690d4c9ed4d28dbb
romeroc1996m <camilaromero.9603@gmail.com>
2024-02-25 22:20:24 -0500

    adding info to study case
    adding info to study case

---
+++ js/studyCase.js
@@ -0,0 +1,13 @@
+
+
+    gsap.registerPlugin(ScrambleTextPlugin);
+
+    let tl = gsap.timeline();
+
+    tl.to("#scramble", {duration:3, scrambleText: {text:"Street Photography", chars:"lowercase", revealDelay: .5}})
+
+
+
+
+
+
